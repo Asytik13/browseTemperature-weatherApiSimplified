080823afeb9b905a2dd483de02f06cd70e3cc8e4
olga <pakhomovaolga96@gmail.com>
2020-12-09 21:08:42 +0100

    edited WeatherController

--- src/main/java/com/example/restservice/WeatherController.java
+++ src/main/java/com/example/restservice/WeatherController.java
@@ -3,42 +3,56 @@ package com.example.restservice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 @RestController
 public class WeatherController {
 
     private List<City> cities = new ArrayList<>();
+    boolean nameExists;
 
     private static final Pattern tempPattern = Pattern.compile("^.*\"temp_c\":\\s*(.+?)\\s*,.*");
 
     @GetMapping("/temperature")
     public Weather greeting(@RequestParam(value = "city", defaultValue = "Krakow") String city){
         Double temperature;
-        String content = "Request returned temperature in "+ city;
+        String content = "Request returned temperature in " + city;
 
-        final String url = "http://api.weatherapi.com/v1/current.json?key=972630bcf041404ba70190228200711&q=" + city;
+        nameExists = cities.stream().anyMatch(x -> x.getName().equals(city));
 
-        try (java.util.Scanner s = new java.util.Scanner(new java.net.URL(url).openStream())) {
-            String resp = s.useDelimiter("\\A").next();
-            Matcher m = tempPattern.matcher(resp);
-            if ( m.find()) {
-                temperature = Double.valueOf(m.group(1));
+        if ( nameExists ) {
+            final String url = "http://api.weatherapi.com/v1/current.json?key=972630bcf041404ba70190228200711&q=" + city;
 
-                long id = cities.stream().filter(x -> x.getName().equals(city))
-                        .findFirst().get().getId();
-                return new Weather(temperature, id, content);
+            try (java.util.Scanner s = new java.util.Scanner(new java.net.URL(url).openStream())) {
+                String resp = s.useDelimiter("\\A").next();
+                Matcher m = tempPattern.matcher(resp);
+                if ( m.find() ) {
+                    temperature = Double.valueOf(m.group(1));
+
+                    long id = cities.stream().filter(x -> x.getName().equals(city))
+                            .findFirst().get().getId();
+                    return new Weather(temperature, id, content);
+                }
+
+
+                throw new RuntimeException("Cannot extract temp from: " + resp);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
-
-            throw new RuntimeException("Cannot extract temp from: " + resp);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
+        else{
+            content = "City isn't defined in a list";
+            temperature = null;
+            long id = 0;
+
+            return  new Weather(temperature, id, content);
+
+
+        }
+
     }
 
     @GetMapping(path = "/citiesList", produces = "application/json")
@@ -54,7 +68,7 @@ public class WeatherController {
 
         String message ="City "+ name + " already exists";
 
-        boolean nameExists = cities.stream().anyMatch(x -> x.getName().equals(name));
+       nameExists = cities.stream().anyMatch(x -> x.getName().equals(name));
 
         if(!nameExists && name!=null) {
             City newCity = new City();
